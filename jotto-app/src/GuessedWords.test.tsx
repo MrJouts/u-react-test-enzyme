8e079a2195fbfc322c3c130840cf47a458bec14b
Emiliano <emiliano.hotes@despegar.com>
2022-09-26 17:24:58 -0300

    added Test if there are no words guessed

--- jotto-app/src/GuessedWords.test.tsx
+++ jotto-app/src/GuessedWords.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, ShallowWrapper } from "enzyme"
 import { findByTestAttr } from "../test/testUtils"
 import GuessedWords from "./GuessedWords"
 
@@ -14,6 +14,23 @@ const setup = (props = {}) => {
   return shallow(<GuessedWords {...setupProps} />)
 }
 
-test('check props', () => {
+describe('if there are no words guessed', () => {
+  let wrapper: ShallowWrapper
+  beforeEach(() => {
+    wrapper = setup({ GuessedWords: [] })
+  })
+
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  })
+
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions")
+    expect(instructions.text().length).not.toBe(0)
+  })
+})
+
+describe('if there are words guessed', () => {
 
 })
