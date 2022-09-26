9f7f344233d9d40159cdd9f2c6a511f75a8dfefb
Emiliano <emiliano.hotes@despegar.com>
2022-09-26 17:59:46 -0300

    Test and code if there are words guessed

--- jotto-app/src/GuessedWords.test.tsx
+++ jotto-app/src/GuessedWords.test.tsx
@@ -3,7 +3,16 @@ import { shallow, ShallowWrapper } from "enzyme"
 import { findByTestAttr } from "../test/testUtils"
 import GuessedWords from "./GuessedWords"
 
-const defaultProps = {
+type GuessedWord = {
+  guessedWord: string;
+  letterMatchCount: number
+}
+
+type Props = {
+  guessedWords: GuessedWord[]
+}
+
+const defaultProps: Props = {
   guessedWords: [
     { guessedWord: 'train', letterMatchCount: 3 }
   ]
@@ -17,11 +26,11 @@ const setup = (props = {}) => {
 describe('if there are no words guessed', () => {
   let wrapper: ShallowWrapper
   beforeEach(() => {
-    wrapper = setup({ GuessedWords: [] })
+    wrapper = setup({ guessedWords: [] })
   })
 
   test("renders without error", () => {
-    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    const component = findByTestAttr(wrapper, "component-guessed-words")
     expect(component.length).toBe(1)
   })
 
@@ -32,5 +41,29 @@ describe('if there are no words guessed', () => {
 })
 
 describe('if there are words guessed', () => {
+  let wrapper: ShallowWrapper
+  const guessedWords: GuessedWord[] = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 },
+  ]
 
+  beforeEach(() => {
+    wrapper = setup({ guessedWords })
+  })
+
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words")
+    expect(component.length).toBe(1)
+  })
+
+  test("renders 'guessed words' section", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1)
+  })
+
+  test("correct number of guessed words", () => {
+    const guessedWordNodes = findByTestAttr(wrapper, "guessed-word")
+    expect(guessedWordNodes.length).toBe(guessedWords.length)
+  })
 })
--- jotto-app/src/GuessedWords.tsx
+++ jotto-app/src/GuessedWords.tsx
@@ -9,13 +9,44 @@ type Props = {
   guessedWords: GuessedWord[]
 }
 
-const GuessedWords = (props: Props) => {
-  const { guessedWords } = props
+const GuessedWords = ({ guessedWords }: Props) => {
+  let contents
+  if (guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">
+        Try to guess the secret word!
+      </span>
+    )
+  } else {
+
+    const guessedWordsRows = guessedWords.map((word, index) => (
+      <tr key={index} data-test="guessed-word">
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ))
+
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guessedWordsRows}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
   return (
     <div data-test="component-guessed-words">
-      {guessedWords?.length &&
-        <div data-test="guess-instructions">Try to guess the word</div>
-      }
+      {contents}
     </div>
   )
 }
