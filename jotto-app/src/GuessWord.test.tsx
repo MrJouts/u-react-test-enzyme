7f3ee1431385354b1466b0b25b9d06c571c04531
Emiliano <emiliano.hotes@despegar.com>
2022-09-29 19:38:57 -0300

    Added more functional tests to GuessWord test file

--- jotto-app/src/GuessWord.test.tsx
+++ jotto-app/src/GuessWord.test.tsx
@@ -30,11 +30,60 @@ describe("no words guessed", () => {
   });
 
   test("creates GuessedWords table with one row", () => {
-    const guessedWOrdRows = findByTestAttr(wrapper, "guessed-row");
+    const guessedWOrdRows = findByTestAttr(wrapper, "guessed-word");
     expect(guessedWOrdRows).toHaveLength(1);
   });
 });
 
-describe("some letters guessed", () => {});
+describe("some words guessed", () => {
+  let wrapper: ReactWrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+  });
 
-describe("guess secret word", () => {});
+  test("creates GuessedWords table with three rows", () => {
+    const guessedWords = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWords).toHaveLength(2);
+  });
+});
+
+describe("guess secret word", () => {
+  let wrapper: ReactWrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+
+    // add value to input box
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    inputBox.simulate("change", { target: { value: "party" } });
+
+    // simulate click on submit button
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+  });
+
+  test("adds row to guessedWords table", () => {
+    const guessedWordNodes = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordNodes).toHaveLength(3);
+  });
+
+  test("displays congrats component", () => {
+    const congrats = findByTestAttr(wrapper, "component-congrats");
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+
+  test("does not display input component contents", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    expect(inputBox.exists()).toBe(false);
+
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    expect(submitButton.exists()).toBe(false);
+  });
+});
